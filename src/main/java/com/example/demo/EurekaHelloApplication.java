f1ef16f9df84cc854e7fd1b328c65db8e7e5e072
Twesh <tweshc@gmail.com>
2018-01-09 15:59:31 -0500

    war packaging

--- src/main/java/com/example/demo/EurekaHelloApplication.java
+++ src/main/java/com/example/demo/EurekaHelloApplication.java
@@ -2,13 +2,31 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaHelloApplication {
+public class EurekaHelloApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaHelloApplication.class, args);
+	}
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EurekaHelloApplication.class);
+    }
+}
+
+@RestController
+class HomeController{
+	
+	@RequestMapping("/home")
+	public String home() {
+		return "Home.";
 	}
 }
